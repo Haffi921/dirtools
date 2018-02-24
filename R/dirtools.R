e6f7665d2b39864bd78698739b1ee0648198a374
Haffi921 <haffi921@gmail.com>
2018-02-24 18:19:44 +0000

    Bug fix: Fixed errors how function get_path works differently between being in RStudio script, RStudio console, running script through Terminal/CMD and running function in Terminal/CMD

--- R/dirtools.R
+++ R/dirtools.R
@@ -6,39 +6,42 @@ get_path <- function() {
 	## We need two solutions:
 	### One when user is using rStudio
 	### and one where the user is not using rStudio
-	isRStudio <- Sys.getenv("RSTUDIO") == "1"
+	isRStudio <- Sys.getenv("RSTUDIO") == 1
 
 	if(isRStudio) {
 		## This function needs the rStudioAPI package to run
-		if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+		if(!requireNamespace("rstudioapi", quietly = TRUE)) {
 			stop("Package \"rstudioapi\" needed for this function to work in RStudio.
 					 Please install it.",
 					 call. = FALSE)
 		}
 
-		rstudioapi::getActiveDocumentContext()$path
-	}
-	else {
-		if(!interactive()) {
-			if(!requireNamespace("here", quietly = TRUE)) {
-				stop("Package \"here\" needed for this function to work in Terminal (Mac/Linux) and CMD (Windows)
-						 Please install it.",
-						 call. = FALSE)
-			}
-			else {
-				sub(".*=", "", commandArgs()[4])
-			}
-		}
-		else {
+		if(rstudioapi::getActiveDocumentContext()$path == "") {
 			switch (Sys.info()[['sysname']],
 				Windows = shell("cd", intern = T),
 				Linux = ,
 				Darwin = {system("pwd", intern = T)},
 				{
 					stop("Could not determine operating system.",
-										call. = FALSE)
+							 call. = FALSE)
 				}
 			)
+		}
+		else {
+			rstudioapi::getActiveDocumentContext()$path
+		}
+	}
+	else {
+		if(!interactive()) {
+			sub(".*=", "", commandArgs()[4])
+		}
+		else {
+			if(!requireNamespace("here", quietly = TRUE)) {
+				stop("Package \"here\" needed for this function to work in Terminal (Mac/Linux) and CMD (Windows)
+						 Please install it.",
+						 call. = FALSE)
+			}
+			here::here()
 		}
 	}
 }
