1f0d94f8f5f0b6080a29743f02924678bf01ddcb
Haffi921 <haffi921@gmail.com>
2018-02-25 21:45:38 +0000

    Temporary testing new function

--- R/dirtools.R
+++ R/dirtools.R
@@ -28,3 +28,33 @@ get_path <- function() {
 		}
 	}
 }
+
+get_this_path() <- function() {
+	isRStudio <- Sys.getenv("RSTUDIO") == 1
+	this_path <- list("path" = "", "filename" = NA)
+
+	if(isRStudio) {
+		## This function needs the rStudioAPI package to run
+		if(!requireNamespace("rstudioapi", quietly = TRUE)) {
+			stop("Package \"rstudioapi\" needed for this function to work in RStudio.
+					 Please install it.",
+					 call. = FALSE)
+		}
+
+		tryCatch(path <- rstudioapi::getSourceEditorContext()$path,
+						 error = function(e) {break()})
+
+		this_path$path <- dirname(path = path)
+		this_path$filename <- basename(path = path)
+	}
+	else if(!interactive()) {
+		path <- gsub("[~+~]+", " ", sub(".*=", "", commandArgs()[4]))
+		this_path$path <- dirname(path = path)
+		this_path$filename <- basename(path = path)
+	}
+	else {
+		this_path$path <- getwd()
+	}
+
+	this_path
+}
