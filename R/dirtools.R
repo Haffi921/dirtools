31d7643ecc6a71e2d41c808f0ddf9fac2c891dc1
Haffi921 <haffi921@gmail.com>
2018-02-26 21:15:03 +0000

    Bug fix: Where full path isn't diplayed while in script

--- R/dirtools.R
+++ R/dirtools.R
@@ -38,7 +38,7 @@ get_this_path <- function() {
 	## Check if this is a script
 	else if(!interactive()) {
 		# Path of current script
-		path <- gsub("[~+~]+", " ", sub(".*=", "", commandArgs()[4]))
+		path <- paste(getwd(), gsub("[~+~]+", " ", sub(".*=", "", commandArgs()[4])), sep = "/")
 
 		dir <- dirname(path)
 		filename <- basename(path)
