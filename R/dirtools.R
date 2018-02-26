08b43589d106af8727637d0c35f809b0d2b1c39d
Haffi921 <haffi921@gmail.com>
2018-02-26 00:34:17 +0000

    Major bug fix; new function 'get_this_filename'

--- R/dirtools.R
+++ R/dirtools.R
@@ -2,40 +2,20 @@
 #'
 #' @keywords get working directory active document path
 
-get_path <- function() {
-	## We need two solutions:
-	### One when user is using rStudio
-	### and one where the user is not using rStudio
-	isRStudio <- Sys.getenv("RSTUDIO") == 1
-
-	if(isRStudio) {
-		## This function needs the rStudioAPI package to run
-		if(!requireNamespace("rstudioapi", quietly = TRUE)) {
-			stop("Package \"rstudioapi\" needed for this function to work in RStudio.
-					 Please install it.",
-					 call. = FALSE)
-		}
-		tryCatch(rstudioapi::getSourceEditorContext()$path, error = function(e) {getwd()})
-	}
-	else {
-		# Script
-		if(!interactive()) {
-			gsub("[~+~]+", " ", sub(".*=", "", commandArgs()[4]))
-		}
-		# Terminal/CMD Console
-		else {
-			getwd()
-		}
-	}
-}
-
 get_this_path <- function() {
-	isRStudio <- Sys.getenv("RSTUDIO") == 1
-
 	this_path <- list("path" = "", "filename" = NA)
+
+	## If function is called in a Terminal/CMD console
+	## or in RStudio console with no open document,
+	## path defaults to current working directory
 	path <- getwd()
 	filename <- NA
 
+	isRStudio <- Sys.getenv("RSTUDIO") == 1
+
+	## We need two solutions:
+	### One when user is using rStudio
+	### and one where the user is not using rStudio
 	if(isRStudio) {
 		## This function needs the rStudioAPI package to run
 		if(!requireNamespace("rstudioapi", quietly = TRUE)) {
@@ -44,14 +24,22 @@ get_this_path <- function() {
 					 call. = FALSE)
 		}
 
+		## If a document is open in RStudio this runs
 		try({
+			# Path of current open document or script that function is called in
 			temp_path <- rstudioapi::getSourceEditorContext()$path
+
 			path <- dirname(temp_path)
 			filename <- basename(temp_path)
-		}, silent = TRUE)
+			},
+			silent = TRUE
+		)
 	}
+	## Check if this is a script
 	else if(!interactive()) {
+		# Path of current script
 		temp_path <- gsub("[~+~]+", " ", sub(".*=", "", commandArgs()[4]))
+
 		path <- dirname(temp_path)
 		filename <- basename(temp_path)
 	}
@@ -60,4 +48,15 @@ get_this_path <- function() {
 	this_path$filename <- filename
 
 	this_path
+}
+
+get_this_filename <- function() {
+	filename <- get_this_path()$filename
+
+	if(is.na(filename)) {
+		stop("Error: This is not a file.", call. = FALSE)
+	}
+	else {
+		filename
+	}
 }
