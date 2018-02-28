cca55848571f56dcc79e45b822877516c6d66392
Haffi921 <haffi921@gmail.com>
2018-02-28 01:18:57 +0000

    testing

--- R/dirtools.R
+++ R/dirtools.R
@@ -43,11 +43,7 @@ get_this_path <- function() {
 		# Path of current script from console
 		path_from_wd <- gsub("[~+~]+", " ", sub(".*=", "", commandArgs()[4]))
 
-		print(path_from_wd)
-		print(substr(sub(" ", "", path_from_wd), 1, nchar(sub(" ", "", wd))))
-		print(wd)
-		print(substr(sub(" ", "", path_from_wd), 1, nchar(sub(" ", "", wd))) != wd && wd != "/")
-		print(substr(sub(" ", "", path_from_wd), 1, nchar(sub(" ", "", wd))) != wd)
+		print(dirname(path_from_wd) == wd)
 
 		if(substr(path_from_wd, 1, nchar(wd)) != wd && wd != "/") {
 			path <- paste(wd, path_from_wd, sep = "/")
